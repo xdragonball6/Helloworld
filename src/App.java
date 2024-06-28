51f42920d1407cc4d7b15ae229ccc46dffa24442
xdragonball6 <xdragonball6@gmail.com>
2024-06-28 15:27:33 +0900

    changes

--- src/App.java
+++ src/App.java
@@ -8,7 +8,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         BoardItemDao boardItemDao = new BoardItemDaoimpl();
 
-
         
         List<Boarditem> boarditems = boardItemDao.selectAll();
         
