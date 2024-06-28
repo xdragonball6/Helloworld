9b88251eb1fe5d61764cf120d84c3a36a4717b26
xdragonball6 <xdragonball6@gmail.com>
2024-06-28 09:18:26 +0900

    Initial Commit

---
+++ test/AppTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+    
+    @Test
+    void sum() {
+        int result = 2 + 3;
+        assertEquals(5, result);
+    }
+}
